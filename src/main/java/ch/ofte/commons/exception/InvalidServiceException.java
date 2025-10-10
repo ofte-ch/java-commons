2aeddc4cdfbc87925cf8706fceb3184fac455bbc
Vy Tien (OFTE) <me@tienminhvy.com>
2025-10-10 15:35:31 +0700

    Add new constructor for the exception class

--- src/main/java/ch/ofte/commons/exception/InvalidServiceException.java
+++ src/main/java/ch/ofte/commons/exception/InvalidServiceException.java
@@ -4,4 +4,8 @@ public class InvalidServiceException extends BaseLogicException {
     public InvalidServiceException(String serviceName) {
         super("The service \"" +  serviceName + "\" cannot be called");
     }
+
+    public InvalidServiceException(String serviceName, String message) {
+        super("The service \"" +  serviceName + "\" cannot be called, reason: " + message);
+    }
 }
